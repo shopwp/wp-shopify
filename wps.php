9e920259942d0729b78278186a96e6290e33ddaa
arobbins <arobbins@simpleblend.net>
2017-06-07 22:18:49 -0500

    Release: 0.1.15

--- wps.php
+++ wps.php
@@ -5,14 +5,14 @@
 WP Shopify
 
 @link              https://wpshop.io
-@since             0.1.14
+@since             0.1.15
 @package           WPS
 
 @wordpress-plugin
 Plugin Name:       WP Shopify
 Plugin URI:        https://wpshop.io
 Description:       Sync your Shopify store with WordPress. Designed to be extensible, seamless, and lightweight.
-Version:           0.1.14
+Version:           0.1.15
 Author:            Andrew Robbins
 Author URI:        https://blog.simpleblend.net
 License:           GPL-2.0+
