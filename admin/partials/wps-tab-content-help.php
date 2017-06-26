b886132df72f6662d86169464e7f50f22c87b7cf
arobbins <arobbins@simpleblend.net>
2017-06-25 23:20:40 -0500

    Removed Tools for now

--- admin/partials/wps-tab-content-help.php
+++ admin/partials/wps-tab-content-help.php
@@ -9,7 +9,7 @@ Tab Content: Help / Debug
 
     <h3 class="wps-admin-section-heading"><span class="dashicons dashicons-sos"></span> Help</h3>
 
-    <p>If you're reading this then you're currently using a beta version of WPS. With all beta's come the inevitable bugs. If you're running into problems don't hesitate to jump into our public Slack to <a href="https://join.slack.com/wpshopify/shared_invite/MTg5OTQxODEwOTM1LTE0OTU5ODY2MTktN2Y1ODk0YzBlNg" target="_blank">ask a question</a>. If Slack isn't your thing, feel free to send us an email outlining your problem to <a href="mailto:hello@wpshop.io">hello@wpshop.io</a></p>
+    <p>If you're running into problems don't hesitate to jump into our public Slack to <a href="https://join.slack.com/wpshopify/shared_invite/MTg5OTQxODEwOTM1LTE0OTU5ODY2MTktN2Y1ODk0YzBlNg" target="_blank">ask a question</a>. If Slack isn't your thing feel free to send us an email outlining your problem to <a href="mailto:hello@wpshop.io">hello@wpshop.io</a></p>
 
     <p>Also make sure to review the documentation found here: <a href="<?php echo $this->config->plugin_env; ?>/docs" target="_blank"><?php echo $this->config->plugin_env; ?>/docs</a></p>
 
