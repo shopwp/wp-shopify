a5d49f259b19d0cf05ad165993eb99799e078194
arobbins <arobbins@simpleblend.net>
2017-07-01 18:10:52 -0500

    Added currency changing and formatting

--- public/partials/collections/single/product.php
+++ public/partials/collections/single/product.php
@@ -2,6 +2,8 @@
   <a href="/products/<?php echo $product->handle; ?>" class="wps-collections-product-link">
     <img src="<?php echo $product->image; ?>" alt="<?php echo $product->title; ?>" class="wps-products-img" />
     <h2 class="wps-collections-product-title wps-products-title"><?php echo $product->title; ?></h2>
-    <h3 class="wps-products-price"><?php echo $product->variants[0]->price; ?></h3>
+    <h3 class="wps-products-price">
+      <?php echo WPS\Utils::wps_format_money($product->variants[0]->price, $product->variants[0]); ?>
+    </h3>
   </a>
 </li>
--- public/partials/products/loop/item-price.php
+++ public/partials/products/loop/item-price.php
@@ -1,3 +1,3 @@
 <h3 class="wps-products-price <?php echo apply_filters( 'wps_products_price_class', '' ); ?>">
-  <?php echo WPS\Utils::wps_format_money($product->price); ?>
+  <?php echo WPS\Utils::wps_format_money($product->price, $product); ?>
 </h3>
