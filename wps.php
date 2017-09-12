52eb20358ce140ba05da7d8f191ae12aa9726908
arobbins <arobbins@simpleblend.net>
2017-09-12 18:00:17 -0500

    Bumped version to 1.0.16

--- wps.php
+++ wps.php
@@ -5,14 +5,14 @@
 WP Shopify
 
 @link              https://wpshop.io
-@since             1.0.15
+@since             1.0.16
 @package           WPS
 
 @wordpress-plugin
 Plugin Name:       WP Shopify
 Plugin URI:        https://wpshop.io
 Description:       Sell your Shopify products on WordPress. A plugin designed to be extensible, seamless, and powerful.
-Version:           1.0.15
+Version:           1.0.16
 Author:            Simpleblend
 Author URI:        https://blog.simpleblend.net
 License:           GPL-2.0+
